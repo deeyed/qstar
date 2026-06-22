4d214a57c95cd53b6c1ecfe00fd5d97968aae406
GungYe <public.gungye@gmail.com>
2026-06-22 14:18:04 +0900

    release(qstar): prepare v0.7.7 beta patch

--- include/qstar/qstar.h
+++ include/qstar/qstar.h
@@ -8,10 +8,10 @@
 #define QSTAR_PATH_MAX 4096
 #endif
 
-#define QSTAR_VERSION "0.7.6-beta"
+#define QSTAR_VERSION "0.7.7-beta"
 #define QSTAR_VERSION_MAJOR 0
 #define QSTAR_VERSION_MINOR 7
-#define QSTAR_VERSION_PATCH 6
+#define QSTAR_VERSION_PATCH 7
 
 struct qstar_string_list {
 	char **items;
